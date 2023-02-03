e1ec1fe22c2896a435bf0fbd166ada0a7555fada
Daniel <Dani_FBN@outlook.com.br>
2023-02-03 11:08:46 -0300

    linter example

---
+++ linter_class/lib/home/domain/usecases/get_products_by_name_and_id_usecase.dart
@@ -0,0 +1,209 @@
+class GetProductsByNameAndIdUsecase {
+  void get({required GetParam param}) {
+    const bool whatever = true;
+
+    if (whatever == true) {}
+
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+
+    print('');
+    print('');
+
+    print('');
+    print('');
+    print('');
+    print('');
+
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    newMethod();
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+
+    print('');
+    print('');
+    print('');
+
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+  }
+
+  void newMethod() {
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+    print('');
+  }
+
+  void _newMethod() {
+    print('');
+  }
+  void _newMethod1() {
+    print('');
+  }
+  void _newMethod2() {
+    print('');
+  }
+  void _newMethod3() {
+    print('');
+  }
+  void _newMethod4() {
+    print('');
+  }
+  void _newMethod5() {
+    print('');
+  }
+  void _newMethod6() {
+    print('');
+  }
+  void _newMethod7() {
+    print('');
+  }
+  void _newMethod8() {
+    print('');
+  }
+  void _newMethod9() {
+    print('');
+  }
+  void _newMethod10() {
+    print('');
+  }
+}
+
+abstract class IParam<T> {
+  Map<String, dynamic> toMap(T value);
+  T fromMap(Map<String, dynamic> map);
+}
+
+class GetParam implements IParam<String> {
+  @override
+  Map<String, dynamic> toMap(String value) {
+    return {};
+  }
+
+  @override
+  String fromMap(Map<String, dynamic> map) {
+    return '';
+  }
+}
+
+class PostParam implements IParam<String> {
+  @override
+  Map<String, dynamic> toMap(String value) {
+    return {};
+  }
+
+  @override
+  String fromMap(Map<String, dynamic> map) {
+    return '';
+  }
+}
---
+++ linter_class/lib/whatever.dart
@@ -0,0 +1,9 @@
+class UserModel {
+  final String id;
+  final String name;
+
+  const UserModel({
+    required this.id,
+    required this.name,
+  });
+}
