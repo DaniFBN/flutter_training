4e4560141b09ef870f21fdb02edead50d32bb95a
Daniel <Dani_FBN@outlook.com.br>
2023-03-17 10:38:03 -0300

    add full_coverage

---
+++ test_class/test/full_coverage_test.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: unused_import
+import 'package:test_class/login.dart';
+import 'package:test_class/main.dart';
+import 'package:test_class/models/user_model.dart';
+import 'package:test_class/repositories/auth_repository.dart';
+import 'package:test_class/utils/exceptions.dart';
+
+void main() {}
